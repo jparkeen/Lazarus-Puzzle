e92178fb49d4bc825fe16d9009c398e3345d0709
aum10 <jparkeen@mail.sfsu.edu>
2017-08-05 16:18:41 -0700

    changed spawn box to array

--- src/commons/SpawnBoxes.java
+++ src/commons/SpawnBoxes.java
@@ -10,8 +10,12 @@ import java.util.TimerTask;
 public class SpawnBoxes extends TimerTask {
 
     private ArrayList<Box> boxes;
-
+    private int boxarray[] = new int[100];
     private Lazarus lazarus;
+    int count = 0;
+
+
+
 
     private static final String[] ALL_BOXES  = {MapReader.CARDBOARD_BOX, MapReader.WOOD_BOX,
             MapReader.STONE_BOX, MapReader.METAL_BOX};
@@ -19,13 +23,27 @@ public class SpawnBoxes extends TimerTask {
     public SpawnBoxes(ArrayList<Box> boxes, Lazarus lazarus) {
         this.boxes = boxes;
         this.lazarus = lazarus;
+
+      for (int i = 0; i < boxarray.length; i++) {
+        Random rand = new Random();
+        int n = rand.nextInt(4);
+        boxarray[i] = n;
+      }
     }
 
     public void run() {
-        // To get a random number for 4 boxes from 1 to 4
-        Random rand = new Random();
-        int boxIndex = rand.nextInt(4);
+        int boxIndex = boxarray[count];
         String boxType = ALL_BOXES[boxIndex];
+
+        count++;
+        int nextBoxIndex = boxarray[count];
+        String nextBoxType = ALL_BOXES[nextBoxIndex];
+
+        boxes.add(new Box(0,700, nextBoxType));
         boxes.add(new Box(lazarus.x, 0, boxType));
+
+
+
+
     }
 }
