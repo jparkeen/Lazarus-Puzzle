0eb0e92ae63839a1be64023d60737d52a2119f34
Richa Mirashi <richamirashi24@gmail.com>
2017-08-05 00:38:27 -0700

    Collision of boxes with other boxes

--- src/commons/MapReader.java
+++ src/commons/MapReader.java
@@ -3,6 +3,9 @@ package commons;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MapReader {
 
@@ -21,6 +24,9 @@ public class MapReader {
     public static String STONE_BOX = "SBX";
 
     public static String METAL_BOX = "MBX";
+
+	public static final Set<String> ALL_BOX_SET = new HashSet<String>(Arrays.asList(MapReader.CARDBOARD_BOX,
+	MapReader.STONE_BOX, MapReader.WOOD_BOX, MapReader.METAL_BOX));;
 
 	public static String[][] readMap(String mapFileName) throws IOException {
 		String[][] array = new String[Globals.MAX_NUMBER_OF_BLOCKS][Globals.MAX_NUMBER_OF_BLOCKS];
